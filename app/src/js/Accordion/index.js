5b30df67bcaabe82a0a2aca9edba16dd16c0fa31
Fu-taKagawa <kagahaya2@gmail.com>
2023-07-15 16:05:45 +0900

    add till contact

--- app/src/js/Accordion/index.js
+++ app/src/js/Accordion/index.js
@@ -1,13 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const title = document.querySelectorAll('.js-accordion-title');
+document.addEventListener('DOMContentLoaded', () => {
+  const accordionItems = document.querySelectorAll('.js-accordion .accordion-item');
 
-  for (let i = 0; i < title.length; i++) {
-    let titleEach = title[i];
-    let content = titleEach.nextElementSibling;
-    titleEach.addEventListener('click', () => {
-      titleEach.classList.toggle('is-active');
-      content.classList.toggle('is-open');
+  accordionItems.forEach(item => {
+    const header = item.querySelector('.accordion-header');
+    const content = item.querySelector('.accordion-content');
+    const arrow = item.querySelector('.accordion-item::after'); // 矢印の要素を取得
+
+    header.addEventListener('click', () => {
+      item.classList.toggle('active');
+      content.style.maxHeight = item.classList.contains('active') ? content.scrollHeight + 'px' : '0';
+
+      if (item.classList.contains('active')) {
+        arrow.style.transform = 'rotate(-45deg)';
+      } else {
+        arrow.style.transform = 'rotate(135deg)';
+      }
     });
-  }
-
+  });
 });
